cc4462f96c6eed6836f5434b4f46fd5c3db31da7
Veniamin Polienko <kasaklalita@mail.ru>
2022-10-19 23:21:26 +0300

    Lesson 1

--- app/src/main/java/com/kasaklalita/learningjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/kasaklalita/learningjetpackcompose/MainActivity.kt
@@ -1,4 +1,4 @@
-git nipackage com.kasaklalita.learningjetpackcompose
+package com.kasaklalita.learningjetpackcompose
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -16,28 +16,35 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            LearningJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            NewText()
+//            LearningJetpackComposeTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colors.background
+//                ) {
+//                    Greeting("Android")
+//                }
+//            }
         }
     }
 }
 
+@Preview (showBackground = true)
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun NewText() {
+    Text(text = "Hello!")
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LearningJetpackComposeTheme {
-        Greeting("Android")
-    }
-}
+//@Composable
+//fun Greeting(name: String) {
+//    Text(text = "Hello $name!")
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun DefaultPreview() {
+//    LearningJetpackComposeTheme {
+//        Greeting("Android")
+//    }
+//}
