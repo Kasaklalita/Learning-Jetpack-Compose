d0e78be6be55c2f1172d5e4cc4461b95a4143f65
Veniamin Polienko <kasaklalita@mail.ru>
2022-10-20 00:58:29 +0300

    Lesson 3

--- app/src/main/java/com/kasaklalita/learningjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/kasaklalita/learningjetpackcompose/MainActivity.kt
@@ -3,69 +3,74 @@ package com.kasaklalita.learningjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.kasaklalita.learningjetpackcompose.ui.theme.LearningJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(
-                modifier = Modifier.background(Color.Gray).fillMaxWidth().fillMaxHeight(0.5f),
-            ) {
-                Column(
-                    modifier = Modifier.background(Color.Red).fillMaxHeight().fillMaxWidth(0.5f),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.SpaceEvenly
-                ) {
-                    Text(text = "Hello!")
-                    Text(text = "Helloasdf!")
-                    Text(text = "Hellsdfasdfsdf!")
-                }
-                Column(
-                    modifier = Modifier.background(Color.Green).fillMaxHeight(0.5f).fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.SpaceEvenly
-                ) {
-                    Text(text = "Hello!")
-                    Text(text = "Helloasdf!")
-                    Text(text = "Hellsdfasdfsdf!")
-                }
+            Column() {
+                ListItem("Veniamin Polienko", "Front End Developer")
+                ListItem("Veniamin Polienko", "Front End Developer")
+                ListItem("Veniamin Polienko", "Front End Developer")
+                ListItem("Veniamin Polienko", "Front End Developer")
             }
-
-
-//            NewText()
-//            LearningJetpackComposeTheme {
-//                // A surface container using the 'background' color from the theme
-//                Surface(
-//                    modifier = Modifier.fillMaxSize(),
-//                    color = MaterialTheme.colors.background
-//                ) {
-//                    Greeting("Android")
-//                }
-//            }
         }
     }
 }
 
-//@Composable
-//fun Greeting(name: String) {
-//    Text(text = "Hello $name!")
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun DefaultPreview() {
-//    LearningJetpackComposeTheme {
-//        Greeting("Android")
-//    }
+@Composable
+private fun ListItem(name: String, prof: String) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = 5.dp
+    ) {
+        Box() {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(64.dp)
+                        .clip(CircleShape),
+                    painter = painterResource(id = R.drawable.ibishopi),
+                    contentDescription = "image",
+                    contentScale = ContentScale.Crop
+                )
+                Column(
+                    modifier = Modifier.padding(start = 16.dp)
+                ) {
+                    Text(text = name)
+                    Text(text = prof)
+                }
+            }
+        }
+    }
+}
+
+//Box(modifier = Modifier.fillMaxSize(),
+//contentAlignment = Alignment.Center) {
+//    Text(text = "Aasdfasf")
 //}
