bc3e188d86f0cbd9c17d1644bbbc7ad3ef505211
Veniamin Polienko <kasaklalita@mail.ru>
2022-10-20 00:36:23 +0300

    Lesson 2

--- app/src/main/java/com/kasaklalita/learningjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/kasaklalita/learningjetpackcompose/MainActivity.kt
@@ -3,12 +3,15 @@ package com.kasaklalita.learningjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.kasaklalita.learningjetpackcompose.ui.theme.LearningJetpackComposeTheme
 
@@ -16,7 +19,31 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NewText()
+            Row(
+                modifier = Modifier.background(Color.Gray).fillMaxWidth().fillMaxHeight(0.5f),
+            ) {
+                Column(
+                    modifier = Modifier.background(Color.Red).fillMaxHeight().fillMaxWidth(0.5f),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(text = "Hello!")
+                    Text(text = "Helloasdf!")
+                    Text(text = "Hellsdfasdfsdf!")
+                }
+                Column(
+                    modifier = Modifier.background(Color.Green).fillMaxHeight(0.5f).fillMaxWidth(),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    Text(text = "Hello!")
+                    Text(text = "Helloasdf!")
+                    Text(text = "Hellsdfasdfsdf!")
+                }
+            }
+
+
+//            NewText()
 //            LearningJetpackComposeTheme {
 //                // A surface container using the 'background' color from the theme
 //                Surface(
@@ -28,12 +55,6 @@ class MainActivity : ComponentActivity() {
 //            }
         }
     }
-}
-
-@Preview (showBackground = true)
-@Composable
-fun NewText() {
-    Text(text = "Hello!")
 }
 
 //@Composable
