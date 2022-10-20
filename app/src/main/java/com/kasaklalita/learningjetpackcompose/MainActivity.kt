2a5ee6b0be669c5ede0b8595658a3aca4a5e0fde
Veniamin Polienko <kasaklalita@mail.ru>
2022-10-20 22:09:53 +0300

    lesson 4

--- app/src/main/java/com/kasaklalita/learningjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/kasaklalita/learningjetpackcompose/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.kasaklalita.learningjetpackcompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -17,6 +19,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,7 +30,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column() {
+            Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                ListItem("Veniamin Polienko", "Front End Developer")
+                ListItem("Veniamin Polienko", "Front End Developer")
+                ListItem("Veniamin Polienko", "Front End Developer")
+                ListItem("Veniamin Polienko", "Front End Developer")
+                ListItem("Veniamin Polienko", "Front End Developer")
+                ListItem("Veniamin Polienko", "Front End Developer")
+                ListItem("Veniamin Polienko", "Front End Developer")
+                ListItem("Veniamin Polienko", "Front End Developer")
                 ListItem("Veniamin Polienko", "Front End Developer")
                 ListItem("Veniamin Polienko", "Front End Developer")
                 ListItem("Veniamin Polienko", "Front End Developer")
@@ -42,7 +53,19 @@ private fun ListItem(name: String, prof: String) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(10.dp),
+            .padding(10.dp)
+            .pointerInput(Unit) {
+//                detectDragGesturesAfterLongPress { change, dragAmount ->
+//                    Log.d("MyLog", "LongPress $")
+//                }
+                                detectTapGestures {
+                                    Log.d("MyLog", "LongPress $")
+                                }
+                detect
+            },
+//            .clickable {
+//                Log.d("MyLog", "Clicked")
+//            }
         shape = RoundedCornerShape(15.dp),
         elevation = 5.dp
     ) {
